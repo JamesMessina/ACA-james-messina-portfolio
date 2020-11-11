fbbc9d04bcbdae7a120ba33fb3988b6203d1966c
James Messina <jmessina17@gmail.com>
2020-11-11 15:33:39 -0600

    interacttive resume

---
+++ resume-interactive.js
@@ -0,0 +1,19 @@
+
+function fadeIn(){
+    document.getElementById('name').classList.add("animate__animated",".animate__fadeIn");
+}
+
+
+function fadeNumberIn(){
+    document.getElementById('number').classList.add("animate__animated",".animate__fadeIn");
+}
+
+
+function disappear(){
+    document.getElementById('john-lennon-pic').style.visibility = 'collapse'; 
+}
+
+function reappear(){
+    document.getElementById('john-lennon-pic').style.visibility = 'inherit';
+}
+
